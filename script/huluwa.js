ff9117aba4805f69b91ecf16dbfa3738935c4936
iSuperman <isSuperman@users.noreply.github.com>
2020-06-01 20:33:55 +0800

    move

---
+++ script/huluwa.js
@@ -0,0 +1,25 @@
+var obj = {
+  "success": true,
+  "code": null,
+  "data": {
+    "allTotalSettlement": 0,
+    "promotionNumbers": [
+      0,
+      0,
+      0,
+      0,
+      0
+    ],
+    "monthNumber": 0,
+    "currentSettlement": 0,
+    "coin": "99990.0000",
+    "promotionNumber": 0,
+    "monthIncome": 0,
+    "coinType": "TATO",
+    "balance": 99990,
+    "otcName": "STOwhite OTC"
+  },
+  "description": null
+}
+
+$done({body: JSON.stringify(obj)});
